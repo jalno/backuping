5a369ac69ffc5f7047e954b3710a957097f78760
Hossein Hosni <hosni.hossein@gmail.com>
2021-08-09 14:15:12 +0430

    #3 Add entrypoint for docker image

---
+++ .docker/docker-entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+BACKUPING_PHP_BIN=/usr/local/bin/php
+BACKUPING_BACKUPING_INDEX_FILE=/var/www/html/index.php
+BACKUPING_BACKUPING_PROCESS=packages/backuping/processes/Backuping
+
+if [ "$1" = "start" ] 
+then
+	/bin/echo -e "[Backuping]: action is: '$1'";
+	export > /backuping-env.save
+
+	if [ -n "$BACKUPING_BACKUP_CRON_EXPRESSION" ]; then
+		echo "[Backuping]: Setup cronjob"
+		echo "$BACKUPING_BACKUP_CRON_EXPRESSION source /backuping-env.save && $H_PHP_BIN $BACKUPING_BACKUPING_INDEX_FILE --process=$BACKUPING_BACKUPING_PROCESS@backup ${BACKUPING_BACKUP_CRON_VERBOSE:+--verbose}";
+		/usr/sbin/crond -f -l 4
+	fi
+
+elif [ "$1" = "backup" ] || [ "$1" = "restore" ] || [ "$1" = "cleanup" ]
+then
+	/bin/echo -e "[Backuping]: action is: '$1'";
+	echo "$H_PHP_BIN $BACKUPING_BACKUPING_INDEX_FILE --process=$BACKUPING_BACKUPING_PROCESS@$1"
+else
+	eval $1
+fi
+
--- Dockerfile
+++ Dockerfile
@@ -5,8 +5,11 @@ COPY . /var/www/html
 RUN mv packages/backuping/.docker/base/config.php packages/base/libraries/config/config.php; \
 	mv packages/backuping/.docker/base/dbObject.php packages/base/libraries/db/dbObject.php; \
 	mkdir -p /docker-entrypoint.d/; \
-	mv packages/backuping/.docker/docker-entrypoint.d/setup-cronjob.sh /docker-entrypoint.d/10-setup-cronjob.sh; \
+	mv packages/backuping/.docker/docker-entrypoint.sh /; \
 	rm -fr packages/dockerize; \
 	find /var/www/html -type d -name ".docker" -prune -exec rm -fr {} \;; \
 	sed -e '/$this->save(/ s/^#*/#/' -i packages/base/libraries/background/Process.php; \
 	sed -e '/$process->save(/ s/^#*/#/' -i packages/base/libraries/router/router.php;
+
+ENTRYPOINT [ "/docker-entrypoint.sh" ]
+CMD [ "start" ]
