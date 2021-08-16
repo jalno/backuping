13043b1e04429a67a871cc5491800b30c60dec27
Hossein Hosni <hosni.hossein@gmail.com>
2021-08-16 16:14:32 +0430

    #3 Remove env file from cronjob

--- .docker/docker-entrypoint.d/setup-cronjob.sh
+++ .docker/docker-entrypoint.d/setup-cronjob.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
-echo "[Backuping]: Check 'BACKUPING_BACKUP_CRON_EXPRESSION' env exists and /backuping-cron-env.save not exists?";
-if [ -n "$BACKUPING_BACKUP_CRON_EXPRESSION" ] && [ ! -f /backuping-cron-env.save ]
+/bin/echo "[Backuping]: Check 'BACKUPING_BACKUP_CRON_EXPRESSION' exists?";
+if [ -n "$BACKUPING_BACKUP_CRON_EXPRESSION" ]
 then
-	/bin/echo "[Backuping]: Setup cronjob (expression: $BACKUPING_BACKUP_CRON_EXPRESSION)";
-	export > /backuping-cron-env.save;
-	/bin/echo "$BACKUPING_BACKUP_CRON_EXPRESSION source /backuping-cron-env.save && /usr/local/bin/php /var/www/html/index.php --process=packages/backuping/processes/Backuping@backup" | crontab -;
+	/bin/echo "[Backuping]: is exists, expression: ($BACKUPING_BACKUP_CRON_EXPRESSION), check is configured before?";
+
+	if ! /bin/grep 'packages/backuping/processes/Backuping@backup' /etc/crontabs/root > /dev/null 2>&1
+	then
+		/bin/echo "[Backuping]: cronjob is not configured, configure backuping cronjob...";
+		/bin/echo "$BACKUPING_BACKUP_CRON_EXPRESSION /usr/local/bin/php /var/www/html/index.php --process=packages/backuping/processes/Backuping@backup" | crontab -;
+	else
+		echo "[Backuping]: cronjob is configured, skip";
+	fi
+
 else
 	/bin/echo "[Backuping]: not exists, skip";
 fi
